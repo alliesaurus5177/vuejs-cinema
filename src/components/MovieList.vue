bb6aaf3149b4a4fd26c81d63d30e51aa8da92e4d
alliesaurus517 <alliesramirez@gmail.com>
2017-11-13 15:27:38 -0600

    refactored into single file components. Adding in Movie Item component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+	<div id="movie-list">
+		<div v-for="movie in filteredMovies" class="movie">{{movie.movie.Title }}</div>
+	</div>
+</template>
+<script>
+
+import genres from '../util/genres'
+
+	export default { 
+			props: ['genre', 'time', 'movies'], 
+			methods: {
+				moviePassesGenreFilter(movie) {
+					if (!this.genre.length) {
+						return true;
+					}
+					else {
+						return this.genre.find(genre => movie.genre === genre);
+					}
+				}
+			},
+			computed: {
+				filteredMovies() {
+					return this.movies.filter(this.moviePassesGenreFilter);
+				}
+			},
+		}
+</script>
