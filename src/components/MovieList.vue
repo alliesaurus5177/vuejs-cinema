54ace67442c6cdeb0a891d750b1e7aacd3245f04
alliesaurus517 <alliesramirez@gmail.com>
2017-11-16 13:48:35 -0600

    to catch home computer up

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,12 +2,21 @@
 
 <template>
 	<div id="movie-list">
-		<div v-for="movie in filteredMovies" class="movie">{{movie.movie.Title }}</div>
+		<div v-if="filteredMovies.length">
+			<movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie" v-bind:sessions="movie.sessions">		</movie-item>		
+		</div>
+		<div v-else-if="movies.length">
+			No Results.
+		</div>
+		<div v-else>
+			Loading...
+		</div>
 	</div>
 </template>
 <script>
 
 import genres from '../util/genres'
+import MovieItem from './MovieItem.vue'
 
 	export default { 
 			props: ['genre', 'time', 'movies'], 
@@ -17,7 +26,14 @@ import genres from '../util/genres'
 						return true;
 					}
 					else {
-						return this.genre.find(genre => movie.genre === genre);
+						let movieGenres = movie.movie.Genre.split(", "); //splits genres at commas
+						let matched = true; 
+						this.genre.forEach(genre => {
+							if (movieGenres.indexOf(genre) === -1) {
+								matched = false; //found a non-present genre in movie 
+							}
+						});
+						return matched;
 					}
 				}
 			},
@@ -26,5 +42,11 @@ import genres from '../util/genres'
 					return this.movies.filter(this.moviePassesGenreFilter);
 				}
 			},
+			components: {
+				MovieItem, 
+			}, 
+			created() {
+				console.log(this.$moment);
+			}
 		}
 </script>
